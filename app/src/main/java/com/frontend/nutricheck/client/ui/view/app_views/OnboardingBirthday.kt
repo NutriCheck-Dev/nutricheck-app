4906541fa6439dd141881c510a592b82eced470f
urllt <159125194+urlltFelix@users.noreply.github.com>
2025-06-25 15:01:55 +0200

    added example for translation

--- app/src/main/java/com/frontend/nutricheck/client/ui/view/app_views/OnboardingBirthday.kt
+++ app/src/main/java/com/frontend/nutricheck/client/ui/view/app_views/OnboardingBirthday.kt
@@ -2,10 +2,14 @@ package com.frontend.nutricheck.client.ui.view.app_views
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.frontend.nutricheck.client.R
+
+
 
 @Composable
 fun Onboardingbirthday(
     modifier: Modifier = Modifier,
-    question: String = "Wann bist du geboren?",
+    question: String = stringResource(id = R.string.onboarding_birthday_question),
     onNextClick: () -> Unit = {}
 ) {}
