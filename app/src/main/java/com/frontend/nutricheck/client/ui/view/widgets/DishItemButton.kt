d163df19ad028ed74b577c3744582422652695da
Marc <mm.mourfih@gmail.com>
2025-07-08 19:33:02 +0200

    Theme vollständig

--- app/src/main/java/com/frontend/nutricheck/client/ui/view/widgets/DishItemButton.kt
+++ app/src/main/java/com/frontend/nutricheck/client/ui/view/widgets/DishItemButton.kt
@@ -16,11 +16,9 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
