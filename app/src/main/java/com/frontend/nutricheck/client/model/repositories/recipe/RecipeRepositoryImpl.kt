7d41a1638c312d1b415a47f78c3ae81009f2c82d
Moritz Münter <mmuenter3004@gmail.com>
2025-07-28 10:45:06 +0200

    Repositories api methods with errorResponse

--- app/src/main/java/com/frontend/nutricheck/client/model/repositories/recipe/RecipeRepositoryImpl.kt
+++ app/src/main/java/com/frontend/nutricheck/client/model/repositories/recipe/RecipeRepositoryImpl.kt
@@ -147,7 +147,7 @@ class RecipeRepositoryImpl @Inject constructor(
             val errorBody = response.errorBody()
 
             if (response.isSuccessful && body != null) {
-                Result.Success(data = TODO())
+                Result.Success(data = TODO()) //toEntity method
             } else if (errorBody != null) {
                 val gson = Gson()
                 val errorResponse = gson.fromJson(
