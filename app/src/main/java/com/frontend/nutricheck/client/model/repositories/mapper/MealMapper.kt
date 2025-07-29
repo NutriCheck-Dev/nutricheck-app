ecca18933027dd952e83ed516a7a64c9e193cd3b
Moritz Münter <mmuenter3004@gmail.com>
2025-07-29 18:43:17 +0200

    Data mapper, but mealMapper unfinished

--- app/src/main/java/com/frontend/nutricheck/client/model/repositories/mapper/MealMapper.kt
+++ app/src/main/java/com/frontend/nutricheck/client/model/repositories/mapper/MealMapper.kt
@@ -1,10 +1,20 @@
 package com.frontend.nutricheck.client.model.repositories.mapper
 
 import com.frontend.nutricheck.client.dto.MealDTO
-import com.frontend.nutricheck.client.model.data_sources.persistence.entity.MealEntity
+import com.frontend.nutricheck.client.model.data_sources.data.Meal
+import java.util.UUID
 
 object MealMapper {
-    fun toEntity(mealDto: MealDTO): MealEntity {
-        TODO("Implement the conversion from MealDTO to Meal")
-    }
+    fun toData(mealDTO: MealDTO): Meal =
+        Meal(
+            id = UUID.randomUUID().toString(),
+            calories = mealDTO.calories,
+            carbohydrates = mealDTO.carbohydrates,
+            protein = mealDTO.protein,
+            fat = mealDTO.fat,
+            date = TODO(),
+            dayTime = TODO(),
+            mealFoodItems = TODO(),
+            mealRecipeItem = TODO()
+        )
 }
