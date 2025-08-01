55f3c96681c8ceb9f35d528d02b3f8c25dda8db2
urllt <159125194+urlltFelix@users.noreply.github.com>
2025-08-01 15:04:17 +0200

    renamed test

--- app/src/test/java/com/nutricheck/frontend/viewmodels/UserDataUtilsLogic.kt
+++ app/src/test/java/com/nutricheck/frontend/viewmodels/UserDataUtilsLogic.kt
@@ -9,7 +9,7 @@ import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import java.util.Date
 
-class UtilsTest {
+class UserDataUtilsLogic {
     @Test
     fun `isBirthdateInvalid with valid birthdate returns false`() {
         // Given
