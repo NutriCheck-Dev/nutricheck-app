6c943f3ec0bf45b3ac92bb9921ddb7c73b61ae16
Marc Minkousse <marcloic.minkousse@gmail.com>
2025-08-21 01:51:20 +0200

    Ingredients can be opened in MealRecipeItems and added an editable flag

--- app/src/main/java/com/frontend/nutricheck/client/ui/view/widgets/ServingsAndServingSizeField.kt
+++ app/src/main/java/com/frontend/nutricheck/client/ui/view/widgets/ServingsAndServingSizeField.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.frontend.nutricheck.client.model.data_sources.data.flags.ServingSize
 
 @Composable
 fun ServingsField(
@@ -38,6 +39,35 @@ fun ServingsField(
         ) {
             Text(
                 text = "$value",
+                color = colors.onSurfaceVariant,
+                style = styles.bodyLarge.copy(fontWeight = FontWeight.SemiBold)
+            )
+        }
+    }
+}
+
+@Composable
+fun ServingSizeField(
+    servingSize: ServingSize
+) {
+    val colors = MaterialTheme.colorScheme
+    val styles = MaterialTheme.typography
+
+    Box(
+        modifier = Modifier
+            .width(180.dp)
+            .background(colors.surfaceVariant, RoundedCornerShape(8.dp))
+            .border(1.dp, colors.outline, RoundedCornerShape(8.dp)),
+        contentAlignment = Alignment.Center
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = servingSize.getDisplayName(),
                 color = colors.onSurfaceVariant,
                 style = styles.bodyLarge.copy(fontWeight = FontWeight.SemiBold)
             )
