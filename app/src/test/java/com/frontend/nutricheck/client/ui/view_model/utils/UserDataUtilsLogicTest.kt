58e533fca9788cae91a1d0d450fa3ce68c96ab95
urllt <159125194+urlltFelix@users.noreply.github.com>
2025-08-23 12:00:24 +0200

    corrected import mistake

--- app/src/test/java/com/frontend/nutricheck/client/ui/view_model/utils/UserDataUtilsLogicTest.kt
+++ app/src/test/java/com/frontend/nutricheck/client/ui/view_model/utils/UserDataUtilsLogicTest.kt
@@ -5,7 +5,7 @@ import com.frontend.nutricheck.client.model.data_sources.data.flags.Gender
 import com.frontend.nutricheck.client.model.data_sources.data.flags.WeightGoal
 import com.frontend.nutricheck.client.model.data_sources.persistence.entity.UserData
 import com.frontend.nutricheck.client.ui.view_model.UserDataUtilsLogic
-import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
 import io.mockk.every
 import io.mockk.mockk
 import org.junit.Test
